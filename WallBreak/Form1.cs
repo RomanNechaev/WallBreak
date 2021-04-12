145f37d3ebad0418d5cf6fb8f87c477f9496219c
Alexandr <ayuniman@mail.ru>
2021-04-12 12:07:58 +0500

    12.04

--- WallBreak/Form1.cs
+++ WallBreak/Form1.cs
@@ -4,43 +4,54 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using WMPLib;
 
 namespace WallBreak
 {
 
     public partial class Form1 : Form
     {
-        public WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
-        
         public Form1()
         {
+            var label = new Label
+            {
+                BackColor = Color.Transparent,
+                Font = new Font("Century Gothic", 80F, ((FontStyle)((FontStyle.Bold | FontStyle.Italic))), GraphicsUnit.Point),
+                Location = new Point(460, 44),
+                Name = "label1",
+                Size = new Size(827, 207),
+                Text = "WallBreak",
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+
+            var startGame = CreateButton("Начать игру", new Point(550, 288));
+            var settings = CreateButton("Настройки", new Point(550, 419));
+            var exit = CreateButton("Выйти из игры", new Point(550, 550));
+            exit.Click += (sender, args) => Application.Exit();
+            
             InitializeComponent();
+            Controls.Add(label);
+            Controls.Add(exit);
+            Controls.Add(startGame);
+            Controls.Add(settings);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private static Button CreateButton(string text, Point coords)
         {
-            Application.Exit();
+            var button = new Button
+            {
+                Text = text,
+                BackColor = SystemColors.Control,
+                FlatStyle = FlatStyle.Flat,
+                Font = new Font("Century Gothic", 20F, FontStyle.Bold, GraphicsUnit.Point),
+                Location = coords,
+                Size = new Size(637, 72),
+            };
+            button.FlatAppearance.MouseOverBackColor = Color.Pink;
+            return button;
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Application.DoEvents();
-        }
-        private void button3_Click(object sender, EventArgs e)
-        {
-            WMP.URL = @"C:\Users\79123\Documents\GitHub\WallBreak\.mp3"; // файл музыкальный
-            WMP.settings.volume = 100; // меняя значение можно регулировать громкость
-            WMP.controls.play(); // Старт
-        }
-       
     }
 }
