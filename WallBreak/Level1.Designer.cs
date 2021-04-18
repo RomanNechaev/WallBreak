20269fe91e48f2814f974d1f3e3d90d140a29231
AlexandrYuniman <ayuniman@mail.ru>
2021-04-18 18:02:19 +0500

    Choose

--- WallBreak/Level1.Designer.cs
+++ WallBreak/Level1.Designer.cs
@@ -32,24 +32,6 @@ namespace WallBreak
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-            
-=======
-            System.ComponentModel.ComponentResourceManager resources =
-                new System.ComponentModel.ComponentResourceManager(typeof(Level1));
-            this.SuspendLayout();
-            // 
-            // Level1
-            // 
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
-            this.Name = "Level1";
-            this.Text = "Level1";
-            this.ResumeLayout(false);
->>>>>>> 639da5f0c9edf8afb0cb8960e017a55f1b4d9f0d
 
             #endregion
         }
