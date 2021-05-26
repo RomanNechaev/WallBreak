5c3f93870d95cb6b2cb9ebbbfd6e07c52970aa53
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2021-05-26 19:04:20 +0500

    asd
    asdads

---
+++ WallBreak/Level222.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WallBreak
+{
+    public class Level222
+    {
+        static Coins coins = new Coins();
+        static Platforms platforms = new Platforms();
+        static Cactuses cactuses = new Cactuses();
+        
+        public List<PictureBox> CactusObject = new List<PictureBox>
+        {
+            cactuses.CreateCactus(1300,953)
+        };
+        
+        public  List<PictureBox> WorldObjects = new List<PictureBox>
+        {
+            platforms.CreatePlatform(160, 900),
+            platforms.CreatePlatform(360, 720),
+            platforms.CreatePlatform(140, 540),
+        };
+
+        public List<PictureBox> CoinsObject = new List<PictureBox>
+        {
+            coins.CreateCoin(800, 900),
+            coins.CreateCoin(190, 850),
+            coins.CreateCoin(410, 670),
+            coins.CreateCoin(180, 490),
+            coins.CreateCoin(820, 725),
+
+        };
+        
+        public readonly int CoinsScore = 5;
+    }
+}
